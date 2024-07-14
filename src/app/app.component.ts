f0234a3ac1729ce242447ffbb693d1f576b6e170
Lee, Keonhyeong <Keonhyeong.Lee@adesso.de>
2024-07-15 00:27:54 +0200

    use file service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpEvent} from "@angular/common/http";
+import {FileService} from "./file.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fileuploadanddownload_front';
+
+  constructor(private fileService: FileService) { }
+
+  onUploadFiles(files: File[]): void {
+    const formData: FormData = new FormData();
+    for(const file of files) {
+      formData.append('files', file, file.name);
+    }
+    this.fileService.upload(formData).subscribe(
+      event => {
+        console.log(event);
+        this.reportProgress(event);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
+
+  onDownloadFiles(filename: string): void {
+    const formData: FormData = new FormData();
+    this.fileService.download(filename).subscribe(
+      event => {
+        console.log(event);
+        this.reportProgress(event);
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
+
+
+  private reportProgress(event: HttpEvent<string[]>) {
+    throw new Error("Method not implemented.");
+  }
 }
