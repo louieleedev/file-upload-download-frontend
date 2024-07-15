7f66eec8f8bcadec0426ccd9d91fd8fff910d14e
Lee, Keonhyeong <Keonhyeong.Lee@adesso.de>
2024-07-15 18:44:55 +0200

    Applikation V.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpEvent} from "@angular/common/http";
-import {FileService} from "./file.service";
-
+import {  HttpErrorResponse, HttpEvent, HttpEventType } from "@angular/common/http";
+import { FileService } from "./file.service";
+import { saveAs } from 'file-saver';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,13 +9,14 @@ import {FileService} from "./file.service";
 })
 export class AppComponent {
 
+  filenames: string[] = [];
+  fileStatus = { status: '', requestType: '', percent: 0 };
+
   constructor(private fileService: FileService) { }
 
   onUploadFiles(files: File[]): void {
-    const formData: FormData = new FormData();
-    for(const file of files) {
-      formData.append('files', file, file.name);
-    }
+    const formData = new FormData();
+    for (const file of files) { formData.append('files', file, file.name); }
     this.fileService.upload(formData).subscribe(
       event => {
         console.log(event);
@@ -28,7 +29,6 @@ export class AppComponent {
   }
 
   onDownloadFiles(filename: string): void {
-    const formData: FormData = new FormData();
     this.fileService.download(filename).subscribe(
       event => {
         console.log(event);
@@ -41,7 +41,43 @@ export class AppComponent {
   }
 
 
-  private reportProgress(event: HttpEvent<string[]>) {
-    throw new Error("Method not implemented.");
+  private reportProgress(httpEvent: HttpEvent<string[] | Blob>): void {
+    switch(httpEvent.type) {
+      case HttpEventType.UploadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total!, 'Uploading... ');
+        break;
+      case HttpEventType.DownloadProgress:
+        this.updateStatus(httpEvent.loaded, httpEvent.total!, 'Downloading... ');
+        break;
+      case HttpEventType.ResponseHeader:
+        console.log('Header returned', httpEvent);
+        break;
+      case HttpEventType.Response:
+        if (httpEvent.body instanceof Array) {
+          this.fileStatus.status = 'done';
+          for (const filename of httpEvent.body) {
+            this.filenames.unshift(filename);
+          }
+        } else {
+          saveAs(new File([httpEvent.body!], httpEvent.headers.get('File-Name')!,
+            {type: `${httpEvent.headers.get('Content-Type')};charset=utf-8`}));
+          // saveAs(new Blob([httpEvent.body!],
+          //   { type: `${httpEvent.headers.get('Content-Type')};charset=utf-8`}),
+          //    httpEvent.headers.get('File-Name'));
+        }
+        this.fileStatus.status = 'done';
+        break;
+      default:
+        console.log(httpEvent);
+        break;
+
+    }
   }
+
+  private updateStatus(loaded: number, total: number, requestType: string): void {
+    this.fileStatus.status = 'progress';
+    this.fileStatus.requestType = requestType;
+    this.fileStatus.percent = Math.round(100 * loaded / total);
+  }
+
 }
--- src/app/file.service.ts
+++ src/app/file.service.ts
@@ -11,16 +11,26 @@ export class FileService {
   constructor(private http: HttpClient) { }
 
   upload(formData: FormData): Observable<HttpEvent<string[]>> {
-    return this.http.post<String[]>(`${this.server}/file/upload`, formData,{
+    return this.http.post<string[]>(`${this.server}/file/upload`, formData,{
+
+      // ermögliche, den Fortschritt des Uploads zu verfolgen
       reportProgress: true,
+
+      // Erhalte Zugriff auf die gesamte Sequenz von HttpEvents, nicht nur den finalen Inhalt
       observe: 'events'
     } );
   }
 
   download(filename: string): Observable<HttpEvent<Blob>> {
     return this.http.get(`${this.server}/file/download/${filename}`,{
+
+      // ermögliche, den Fortschritt des Uploads zu verfolgen
       reportProgress: true,
+
+      // Erhalte Zugriff auf die gesamte Sequenz von HttpEvents, nicht nur den finalen Inhalt
       observe: 'events',
+
+      // teile Angular mit, dass die erwartete Antwort ein Blob-Objekt ist, das typischerweise für binäre Daten wie Dateien verwendet wird.
       responseType: 'blob'
     } );
   }
